7cde7482b6a4f98a20176d75add109c8ef0cf830
Victor Domiciano <victor.dm3@gmail.com>
2019-11-06 12:43:01 -0300

    Debug godotrateit for iOS 10.2 or previous versions

--- ios/godotRateIt.mm
+++ ios/godotRateIt.mm
@@ -28,16 +28,20 @@ void GodotRateIt::do_ios_rate(const String &app_id) {
         [SKStoreReviewController requestReview];
         return;
     }
+
+    NSString *app_id_nsstring = [[[NSString alloc] initWithUTF8String:app_id.utf8().get_data()] autorelease];
     NSString *str;
+
     float ver = [[[UIDevice currentDevice] systemVersion] floatValue];
     if (ver >= 7.0 && ver < 7.1) {
-        str = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/id%@", &app_id];
+        str = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/id=%@", app_id_nsstring];
     } else if (ver >= 8.0) {
-        str = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/Webcts/MZStore.woa/wa/viewContentsUserReviews?id=%@&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software", &app_id];
+        str = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/Webcts/MZStore.woa/wa/viewContentsUserReviews?id=%@&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software", app_id_nsstring];
     } else {
-        str = [NSString stringWithFormat:@"itms-apps://ax.itunes.apple.com/Webcts/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@", &app_id];
+        str = [NSString stringWithFormat:@"itms-apps://ax.itunes.apple.com/Webcts/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@", app_id_nsstring];
     }
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:str]];
+
 }
 
 void GodotRateIt::_bind_methods() {
